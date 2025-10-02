990b42b1451b5130fa0154fd927ba38ab5e44863
sonia1707 <soniajarodine@gmail.com>
2025-10-02 21:14:19 +0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,20 +1,16 @@
-// fichier: utils.c
 #include <stdio.h>
-
 // Fonction pour échanger deux éléments
 void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
 }
-
 // Fonction pour afficher le tableau
 void printArray(int arr[], int n) {
     for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
-
 // Fonction pour vérifier si le tableau est trié 
 int isSorted(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
