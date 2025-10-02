b4d0e514eb33ab8a74ad60f8319d74b65ae1df21
sonia1707 <soniajarodine@gmail.com>
2025-10-02 21:17:25 +0300

    Update heapsort.c
    Algorithme de tri principal

--- heapsort.c
+++ heapsort.c
@@ -1,1 +1,18 @@
-
+#include "utils.h"
+// Fonction principale de tri par tas
+void heapsort(int arr[], int n) {
+ // Étape 1 : Construction du tas max
+    buildMaxHeap(arr, n);
+     printf("Après construction du tas max: ");
+    printArray(arr, n);
+// Étape 2 : Extraction des éléments un par un
+    for (int i = n - 1; i > 0; i--) {
+ // Déplacer la racine (max) à la fin
+        swap(&arr[0], &arr[i]);
+ // Appeler heapify sur le tas réduit
+        heapify(arr, i, 0);
+// Affichage intermédiaire (bonus)
+        printf("Après extraction %d: ", n - i);
+        printArray(arr, n);
+    }
+}
