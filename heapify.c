5ff3c73b9d94ec62322f2d14cbca682cfafb53f5
Romance197 <antsaromance@gmail.com>
2025-10-07 21:05:24 +0300

    Update heapify.c

--- heapify.c
+++ heapify.c
@@ -5,15 +5,3 @@ void heapify(int arr[], int n, int i) {
     int largest = i;          // Racine
     int left = 2 * i + 1;     // Enfant gauche
     int right = 2 * i + 2;    // Enfant droit
-// Si l'enfant gauche est plus grand que la racine
-    if (left < n && arr[left] > arr[largest])
-        largest = left;
- // Si l'enfant droit est plus grand que le plus grand actuel
-    if (right < n && arr[right] > arr[largest])
-        largest = right;
-// Si le plus grand n'est pas la racine
-    if (largest != i) {
-        swap(&arr[i], &arr[largest]);
-        heapify(arr, n, largest); // Récursivement
-    }
-}
