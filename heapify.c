897c1bd5df995e1fe5f26eabbbb1e2c08d49c79d
Romance197 <antsaromance@gmail.com>
2025-10-08 13:36:12 +0300

    Update heapify.c

--- heapify.c
+++ heapify.c
@@ -1,1 +1,7 @@
-
+#include "utils.h"
+#include "heapify.h"
+// Fonction pour rétablir la propriété du tas (heapify)
+void heapify(int arr[], int n, int i) {
+    int largest = i;          // Racine
+    int left = 2 * i + 1;     // Enfant gauche
+    int right = 2 * i + 2;    // Enfant droit
