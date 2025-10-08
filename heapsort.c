138647673255686a4562d353d638dedd06ce258f
sonia1707 <soniajarodine@gmail.com>
2025-10-08 17:25:08 +0300

    Update heapsort.c

--- heapsort.c
+++ heapsort.c
@@ -5,17 +5,19 @@
 #include "heapify.h"
 // Fonction principale de tri par tas
 void heapsort(int arr[], int n) {
- // Étape 1 : Construction du tas max
-    buildMaxHeap(arr, n);
-     printf("Après construction du tas max: ");
+    // Étape 1 : Construction du tas max
+    build_heap(arr, n);
+    printf("Après construction du tas max: ");
     printArray(arr, n);
-// Étape 2 : Extraction des éléments un par un
+    // Étape 2 : Extraction des éléments un par un
     for (int i = n - 1; i > 0; i--) {
- // Déplacer la racine (max) à la fin
+        // Déplacer la racine (max) à la fin
         swap(&arr[0], &arr[i]);
- // Appeler heapify sur le tas réduit
+
+        // Appeler heapify sur le tas réduit
         heapify(arr, i, 0);
-// Affichage intermédiaire (bonus)
+
+        // Affichage intermédiaire (bonus)
         printf("Après extraction %d: ", n - i);
         printArray(arr, n);
     }
